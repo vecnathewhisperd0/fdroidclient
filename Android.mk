f496581db76cdeb8bb75bcb8ce04283e2d21172d
Saleem Rashid <spudowiar@outlook.com>
2016-03-30 19:38:28 +0100

    Android.mk: less hacky solution

--- Android.mk
+++ Android.mk
@@ -4,23 +4,19 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := F-Droid
 LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
 LOCAL_PACKAGE_NAME := F-Droid
 
 fdroid_root  := $(LOCAL_PATH)
 fdroid_dir   := app
-fdroid_out   := $(PWD)/$(OUT_DIR)/target/common/obj/APPS/$(LOCAL_MODULE)_intermediates
-fdroid_build := $(fdroid_root)/$(fdroid_dir)/build
-fdroid_apk   := build/outputs/apk/$(fdroid_dir)-release-unsigned.apk
+fdroid_out   := $(call intermediates-dir-for,$(LOCAL_MODULE_CLASS),$(LOCAL_MODULE))
+fdroid_apk   := $(fdroid_out)/$(fdroid_dir)/outputs/apk/$(fdroid_dir)-release-unsigned.apk
 
-$(fdroid_root)/$(fdroid_dir)/$(fdroid_apk):
-	rm -Rf $(fdroid_build)
-	mkdir -p $(fdroid_out)
-	ln -sf $(fdroid_out) $(fdroid_build)
-	cd $(fdroid_root)/$(fdroid_dir) && gradle assembleRelease
+$(fdroid_apk):
+	gradle -Dbuild.dir=$(fdroid_out) -p$(fdroid_root)/$(fdroid_dir) assembleRelease
 
 LOCAL_CERTIFICATE := platform
-LOCAL_SRC_FILES := $(fdroid_dir)/$(fdroid_apk)
-LOCAL_MODULE_CLASS := APPS
+LOCAL_PREBUILT_MODULE_FILE := $(fdroid_apk)
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 
 include $(BUILD_PREBUILT)
