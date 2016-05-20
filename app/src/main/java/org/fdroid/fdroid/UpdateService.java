019e7624722f403eda581a4bea5d4f1a755df303
Liam Symonds <liam@hub95.com>
2016-05-20 21:20:27 +0100

    Revert "- Add get method to Preferences.java to retrieve the update period in milliseconds according to the value that the user chooses."
    This reverts commit e0085cac8dc61ca52dba82ba10305235a01de35f.

--- app/src/main/java/org/fdroid/fdroid/UpdateService.java
+++ app/src/main/java/org/fdroid/fdroid/UpdateService.java
@@ -107,23 +107,27 @@ public class UpdateService extends IntentService {
      * is changed, or c) on startup, in case we get upgraded.
      */
     public static void schedule(Context ctx) {
-        long updateIntervalInMillis = Preferences.get().getUpdateIntervalInMillis();
+
+        SharedPreferences prefs = PreferenceManager
+                .getDefaultSharedPreferences(ctx);
+        String sint = prefs.getString(Preferences.PREF_UPD_INTERVAL, "0");
+        int interval = Integer.parseInt(sint);
+
         Intent intent = new Intent(ctx, UpdateService.class);
         PendingIntent pending = PendingIntent.getService(ctx, 0, intent, 0);
 
         AlarmManager alarm = (AlarmManager) ctx
                 .getSystemService(Context.ALARM_SERVICE);
         alarm.cancel(pending);
-
-        if (updateIntervalInMillis > 0) {
+        if (interval > 0) {
             alarm.setInexactRepeating(AlarmManager.ELAPSED_REALTIME,
                     SystemClock.elapsedRealtime() + 5000,
-                    updateIntervalInMillis,
-                    pending);
+                    AlarmManager.INTERVAL_HOUR, pending);
             Utils.debugLog(TAG, "Update scheduler alarm set");
         } else {
             Utils.debugLog(TAG, "Update scheduler alarm not set");
         }
+
     }
 
     @Override
