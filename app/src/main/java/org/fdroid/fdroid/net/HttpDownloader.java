a290788cc8d286dc59ff3721556c8a817c522ff9
Janne Koschinski <janne@kuschku.de>
2018-05-07 11:06:43 +0200

    Automatically follow redirects
    Currently Repositories added via fdroidrepo:// URL automatically get a
    http:// URL, but the HttpDownloader does not follow redirects.
    
    This patch adds functionality to automatically follow redirects for all
    HTTP 3xx codes, following up to 20 redirects. This information is
    currently not cached as the redirect path of a repo may change over time
    
    Signed-off-by: Janne Koschinski <janne@kuschku.de>

--- app/src/main/java/org/fdroid/fdroid/net/HttpDownloader.java
+++ app/src/main/java/org/fdroid/fdroid/net/HttpDownloader.java
@@ -137,22 +137,35 @@ public class HttpDownloader extends Downloader {
     }
 
     private HttpURLConnection getConnection() throws SocketTimeoutException, IOException {
-        HttpURLConnection connection;
-        if (isSwapUrl(sourceUrl)) {
-            // swap never works with a proxy, its unrouted IP on the same subnet
-            connection = (HttpURLConnection) sourceUrl.openConnection();
-        } else {
-            connection = NetCipher.getHttpURLConnection(sourceUrl);
-        }
+        HttpURLConnection connection = null;
+        URL url = sourceUrl;
+        int redirectCounter = 0;
+        do {
+            if (connection != null) {
+                url = new URL(connection.getHeaderField("Location"));
+            }
 
-        connection.setRequestProperty("User-Agent", "F-Droid " + BuildConfig.VERSION_NAME);
-        connection.setConnectTimeout(getTimeout());
+            if (isSwapUrl(url)) {
+                // swap never works with a proxy, its unrouted IP on the same subnet
+                connection = (HttpURLConnection) url.openConnection();
+            } else {
+                connection = NetCipher.getHttpURLConnection(url);
+            }
 
-        if (username != null && password != null) {
-            // add authorization header from username / password if set
-            String authString = username + ":" + password;
-            connection.setRequestProperty("Authorization", "Basic " + Base64.toBase64String(authString.getBytes()));
-        }
+            connection.setRequestProperty("User-Agent", "F-Droid " + BuildConfig.VERSION_NAME);
+            connection.setConnectTimeout(getTimeout());
+
+            if (username != null && password != null) {
+                // add authorization header from username / password if set
+                String authString = username + ":" + password;
+                connection.setRequestProperty("Authorization", "Basic " + Base64.toBase64String(authString.getBytes()));
+            }
+        } while (
+          // Response Codes 300 - 399 are all redirects
+          connection.getResponseCode() >= 300 &&
+          connection.getResponseCode() < 400 &&
+          // Only follow at most 20 redirects (default taken from Firefox)
+          redirectCounter++ < 20);
         return connection;
     }
 
